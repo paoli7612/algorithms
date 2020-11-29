36f2fbfddfdcf591f6fe8b77f3db19cbc1a3437a
Tommaso Paoli <paoli7612@gmail.com>
2020-11-29 16:02:44 +0100

    huffman 2

--- huffman/main.cc
+++ huffman/main.cc
@@ -18,10 +18,25 @@ int main(int argc, char** argv){
 	
 	for (int i=0; i<n; i++){
 		char c = message[i];
+		cout << c;
+		int p;
+		for (p=0; p<num_caratteri; p++)
+			if (caratteri[p] == c) break;
+		if (p == num_caratteri){
+			caratteri[p] = c;
+			ripetizioni[p] = 1;
+			num_caratteri++;
+		} else {
+			ripetizioni[p]++;
+		}
 		// se è gia stato salvato
 			// incrementa il numero di ripetizioni
 		// altrimenti
 			// salvalo con ripetizioni = 0
+	}
+	cout << endl;
+	for (int i=0; i<num_caratteri; i++){
+		cout << "(" << caratteri[i] << " x " << ripetizioni[i] << ")" << endl;
 	}
 	return 0;
 }
