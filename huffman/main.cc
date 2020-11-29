f0aa6d78b058e530cd09378d295910e795931417
Tommaso Paoli <paoli7612@gmail.com>
2020-11-29 16:24:20 +0100

    huffman 4

--- huffman/main.cc
+++ huffman/main.cc
@@ -9,12 +9,7 @@ int main(int argc, char** argv){
 	cout << "inserisci la parola da comprimere (usa _ per separare più sottoparole (max 256 caratteri)" << endl;
 	cin >> message;
 	int n = 256;
-	//cout << "Inserisci il numero sequenza caratteri da comprimere: ";
-	//cin >> n;
-	
-	//char* message = new char[n];
-	//cin >> message;
-		
+			
 	char caratteri[n];
 	int ripetizioni[n];
 	int num_caratteri = 0; 
@@ -23,9 +18,11 @@ int main(int argc, char** argv){
 	
 	for (int i=0; i<n; i++){
 		char c = message[i];
+		if (c == ' ') break;
 		int p;
 		for (p=0; p<num_caratteri; p++)
-			if (caratteri[p] == c) break;
+			if (caratteri[p] == c)
+				break;
 		if (p == num_caratteri){
 			caratteri[p] = c;
 			ripetizioni[p] = 1;
@@ -34,9 +31,22 @@ int main(int argc, char** argv){
 			ripetizioni[p]++;
 		}
 	}
-	cout << endl;
-	for (int i=0; i<num_caratteri; i++){
-		cout << "(" << caratteri[i] << " x " << ripetizioni[i] << ")" << endl;
+	cout << endl << "num_caratteri: " << num_caratteri << endl;
+
+	
+	// ordiniamo i vettori di "caratteri" e "ripetizioni" in base al numero di volte che il carattere si ripete
+	int tot = num_caratteri-1;
+	char cc[tot];
+	int rr[tot];
+	for (int i=0; i<tot; i++){
+		cc[i] = caratteri[i];
+		rr[i] = ripetizioni[i];
 	}
+	
+	for (int i=0; i<tot; i++){
+		cout << "(" << cc[i] << " x " << rr[i] << ")" << endl;
+	}
+	
+		
 	return 0;
 }
