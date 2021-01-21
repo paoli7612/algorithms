fecd2bcec9b9b27cedb8a284252a96cc63c1f79b
Tommaso Paoli <paoli7612@gmail.com>
2021-01-21 14:52:03 +0100

    programmazione 2

---
+++ Grafi/grafo/grafo.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "grafo.h"
+
+void aggiungi_arco(grafo_t &grafo, const int a, const int b, float peso){
+	
+}
+
+int main(int argc, char** argv)
+{
+	grafo_t grafo = nuovo_grafo(4);
+	grafo_t grafo2 = nuovo_grafo(6);
+	
+	stampa(grafo);
+	stampa(grafo2);
+	
+	return 0;	
+}
