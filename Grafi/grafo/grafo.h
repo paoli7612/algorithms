1d063def64f48b06048c498483273a95002d4ffa
Tommaso Paoli <paoli7612@gmail.com>
2021-01-22 01:38:36 +0100

    non è

--- Grafi/grafo/grafo.h
+++ Grafi/grafo/grafo.h
@@ -1,54 +1,33 @@
+int numero_casuale(){
+	return rand()%10;
+}
+
 typedef struct adj_t {
-	int nodo;
-	float peso;
-	adj_t *next;
+
 }* lista;
 
+void aggiungi_adj(adj_t *adj, const int nodo, const float peso, bool is_first=true)
+{
+
+}
+
 struct grafo_t {
-	int *vertici;
-	adj_t *adj;
-	int dim;
+
 };
 
 grafo_t nuovo_grafo(const int dim)
 {
-	grafo_t grafo;
-	
-	grafo.dim = dim;
-	grafo.adj = new adj_t[dim];
-	grafo.vertici = new int[dim];
-	
-	for (int i=0; i<dim; i++){
-		grafo.vertici[i] = 0;
-		grafo.adj[i];
-	}
-	
-	return grafo;
+
 }
 
-void stampa(const adj_t *adj)
+using namespace std;
+
+void stampa(adj_t *adj)
 {
-		
+
 }
 
 void stampa(const grafo_t &grafo)
 {
-	using namespace std;
 
-	cout << "Grafo___" << endl;
-	cout << "\tdim: " << grafo.dim << endl;
-	
-	cout << "\tvertici: "; 
-	for (int i=0; i<grafo.dim; i++)
-		cout << grafo.vertici[i] << " ";
-	cout << endl;
-	
-	cout << "\tadj: ";
-	for (int i=0; i<grafo.dim; i++){
-		cout << endl << "\t\t";
-		cout << grafo.vertici[i] << ": ";
-		stampa(&grafo.adj[i]);		
-	}
-	
-	cout << endl << "_______________" << endl;
 }
