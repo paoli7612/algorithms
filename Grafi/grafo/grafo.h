f94c430d400e8620fc86733a383e4773a0b20e3d
Tommaso Paoli <paoli7612@gmail.com>
2021-01-22 02:07:03 +0100

    step 2

--- Grafi/grafo/grafo.h
+++ Grafi/grafo/grafo.h
@@ -3,31 +3,53 @@ int numero_casuale(){
 }
 
 typedef struct adj_t {
-
-}* lista;
-
-void aggiungi_adj(adj_t *adj, const int nodo, const float peso, bool is_first=true)
-{
-
-}
+	int *vertice;
+	adj_t *next;
+}* lista_adj;
 
 struct grafo_t {
-
+	int *vertici;
+	lista_adj *adj;
+	int dim;
 };
+
+void aggiungi(lista_adj &adj, int *vertice)
+{
+	if (adj == NULL)
+	{
+		adj = new adj_t;
+		adj->vertice = vertice;
+		adj->next = NULL;
+	}
+	else
+	{
+		aggiungi(adj->next, vertice);
+	}
+}
+
+void collega_a(grafo_t grafo, const int a, const int b)
+{
+	aggiungi(grafo.adj[a], &grafo.vertici[b]);
+}
 
 grafo_t nuovo_grafo(const int dim)
 {
+	grafo_t grafo;
 
+	grafo.dim = dim;
+	grafo.vertici = new int[dim];
+	grafo.adj = new lista_adj[dim];
+
+	for (int i=0; i<dim; i++)
+	{
+		grafo.vertici[i] = numero_casuale();
+		grafo.adj[i] = NULL;
+		int n = rand()%5;
+		for (int j=0; j<n; j++)
+			collega_a(grafo, i, rand()%dim);
+	}
+
+	return grafo;
 }
 
-using namespace std;
 
-void stampa(adj_t *adj)
-{
-
-}
-
-void stampa(const grafo_t &grafo)
-{
-
-}
