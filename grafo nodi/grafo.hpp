f211fa52ca04e5ddc67882bb6caba05ffd518041
Tommaso Paoli <paoli7612@gmail.com>
2021-01-22 03:40:24 +0100

    test 1 e 2

--- grafo nodi/grafo.hpp
+++ grafo nodi/grafo.hpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <fstream>
 
 #define MAX_VERTICI 10
 #define MAX_ARCHI 20
 
 using namespace std;
+
+const char FILENAME[] = "grafo.txt";
 
 struct vertice_t {
     int value;
@@ -21,8 +24,10 @@ struct grafo_t {
     int n_archi;
 };
 
+void collega_vertici(grafo_t &, const int, const int);
+
 //________________________________________________NUOVO
-grafo_t nuovo()
+grafo_t nuovo_grafo()
 {
     grafo_t grafo;
 
@@ -32,7 +37,7 @@ grafo_t nuovo()
     return grafo;
 }
 
-vertice_t nuovo(const int value)
+vertice_t nuovo_vertice(const int value)
 {
     vertice_t vertice;
 
@@ -41,7 +46,7 @@ vertice_t nuovo(const int value)
     return vertice;
 }
 
-arco_t nuovo(vertice_t *x, vertice_t *y)
+arco_t nuovo_arco(vertice_t *x, vertice_t *y)
 {
     arco_t arco;
 
@@ -53,7 +58,7 @@ arco_t nuovo(vertice_t *x, vertice_t *y)
 
 
 //________________________________________________AGGIUNGI
-bool aggiungi(grafo_t &grafo, const vertice_t &vertice)
+bool aggiungi_vertice(grafo_t &grafo, const vertice_t &vertice)
 {
     if (grafo.n_vertici == MAX_VERTICI)
         return false;
@@ -62,7 +67,7 @@ bool aggiungi(grafo_t &grafo, const vertice_t &vertice)
     return true;
 }
 
-bool aggiungi(grafo_t &grafo, const arco_t arco)
+bool aggiungi_arco(grafo_t &grafo, const arco_t arco)
 {
     if (grafo.n_archi == MAX_ARCHI)
         return false;
@@ -72,30 +77,82 @@ bool aggiungi(grafo_t &grafo, const arco_t arco)
 }
 
 //________________________________________________STAMPA
-void stampa(vertice_t vertice)
+void stampa_vertice(vertice_t vertice, ostream &out=cout, bool is_file=false)
 {
-    cout << " " << vertice.value << " ";
+    out << vertice.value << " ";
 }
 
-void stampa(arco_t arco)
+void stampa_arco(arco_t arco, ostream &out=cout, bool is_file=false)
 {
-    cout << " " << arco.x->value << " -> " << arco.y->value << " ";
+    if (is_file)
+        out << arco.x->value << " " << arco.y->value << endl;
+    else 
+        out << " " << arco.x->value << " -> " << arco.y->value << " ";
 }
 
-void stampa(grafo_t grafo)
+void stampa_grafo(grafo_t grafo, ostream &out=cout, bool is_file=false)
 {
+    if (is_file)
+        out << grafo.n_vertici << endl;
     for (int i=0; i<grafo.n_vertici; i++)
-        stampa(grafo.vertici[i]);
+        stampa_vertice(grafo.vertici[i], out, is_file);
 
-    cout << endl;
+    out << endl;
 
+    if (is_file)
+        out << grafo.n_archi << endl;
     for (int i=0; i<grafo.n_archi; i++)
     {
-        stampa(grafo.archi[i]);
+        stampa_arco(grafo.archi[i], out, is_file);
         cout << endl;
     }
+}
 
-    cout << endl;
+//________________________________________________SALVA
+
+bool salva(grafo_t &grafo)
+{
+    ofstream file(FILENAME);
+
+    if (!file)
+        return false;
+
+    stampa_grafo(grafo, file, true);
+
+    file.close();
+    return static_cast<bool>(file);
+}
+
+bool carica(grafo_t &grafo)
+{
+    grafo.n_archi = 0;
+    grafo.n_vertici = 0;
+    ifstream file(FILENAME);
+
+    if (!file)
+        return false;
+    
+    int m; // n_vertici
+    file >> m;
+    for (int i=0; i<m; i++)
+    {
+        int a;
+        file >> a;
+        aggiungi_vertice(grafo, nuovo_vertice(a));
+    }
+    
+    int n; // n_archi
+    file >> n;
+    for (int i=0; i<n; i++)
+        {
+            int a, b;
+            file >> a >> b;
+            collega_vertici(grafo, a, b);
+        }
+
+
+    file.close();
+    return static_cast<bool>(file);
 }
 
 //________________________________________________SPECIAL
@@ -105,8 +162,8 @@ void collega_vertici(grafo_t &grafo, const int a, const int b)
 {
     vertice_t *x = &grafo.vertici[a];
     vertice_t *y = &grafo.vertici[b];
-    arco_t arco = nuovo(x, y);
-    aggiungi(grafo, arco);
+    arco_t arco = nuovo_arco(x, y);
+    aggiungi_arco(grafo, arco);
 }
 
 /* A partire dal vertice ad indice a stampa tutti i vertici collegati */
@@ -116,7 +173,7 @@ void stampa_percorso(grafo_t &grafo, const int a, int max=10)
         return;
 
     vertice_t *x = &grafo.vertici[a];
-    stampa(*x);
+    stampa_vertice(*x, cout, false);
 
     for (int i=0; i<grafo.n_archi; i++)
     {
--- grafo nodi/main.cc
+++ grafo nodi/main.cc
@@ -5,24 +5,40 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
+void test1()
 {
-
-    grafo_t grafo = nuovo();
+    grafo_t grafo = nuovo_grafo();
 
     for (int i=0; i<5; i++)
-    {
-        vertice_t vertice = nuovo(i);
-        aggiungi(grafo, vertice);
-    }
+        aggiungi_vertice(grafo, nuovo_vertice(i));
+    
     
     collega_vertici(grafo, 0, 1);
     collega_vertici(grafo, 1, 3);
     collega_vertici(grafo, 3, 4);
+    collega_vertici(grafo, 4, 2);
 
-    stampa(grafo);
 
+    stampa_grafo(grafo);
+
+    cout << "Primo persorso da 0: ";
     stampa_percorso(grafo, 0);
+
+    salva(grafo);
+}
+
+void test2()
+{
+    grafo_t grafo;
+
+    carica(grafo);
+    stampa_grafo(grafo);
+}
+
+int main(int argc, char** argv)
+{
+
+    test1();
 
     return 0;
 
