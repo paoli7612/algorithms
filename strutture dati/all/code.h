ec364c8b96b228b38890102b400bbbbdf33312fa
paoli612 <paoli7612@gmail.com>
2021-02-04 09:34:34 +0100

    make file test

---
+++ strutture dati/all/code.h
@@ -0,0 +1,15 @@
+struct coda_t {
+    int *vettore;
+    int lunghezza;
+    int elementi;
+};
+
+void aggiungi(coda_t coda, int numero)
+{
+
+}
+
+void stampa(const coda_t coda)
+{
+    
+}
--- strutture dati/all/main.cc
+++ strutture dati/all/main.cc
@@ -1,18 +1,15 @@
 #include <iostream>
 #include <ctime>
-#include <stdlib.h>
-#include <stdio.h>
+
+#include "code.h"
 
 using namespace std;
-
-#include "all.hpp"
 
 int main(int argc, char** argv)
 {
     srand(time(NULL));
-
-    cout << time(NULL) << endl;
-    coda_t coda = NULL;
+    
+    coda_t coda;
 
     for (int i=0; i<10; i++){
         aggiungi(coda, rand()%10);
