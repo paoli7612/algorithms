1ddba987aeb87d6fab759b7210d9b565d2606e89
paoli7612 <paoli7612@gmail.com>
2021-02-06 19:19:52 +0100

    vector

--- DFS/2.cpp
+++ DFS/2.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <ctime>
 #include <vector>
 
 using namespace std;
 
+
 int main(int argc, char** argv)
 {
+  srand(time(NULL));
+
+  std::vector<int> v;
+  v.begin();
+
+  for (int n=0; n<10; n++)
+    v.push_back(rand()%10);
+  for (int n=0; n<10; n++)
+    cout << v[n] << endl;
 
   return 0;
 }
