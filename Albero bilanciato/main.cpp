47c416c46a498f78c4ee2db80ae4d46602861670
Tommaso Paoli <paoli7612@gmail.com>
2021-02-10 22:21:58 +0100

    famo tutto qua

--- Albero bilanciato/main.cpp
+++ Albero bilanciato/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include <stdlib.h>
 #include <cmath>
 #include <ctime>
+#include <stdlib.h>
+
 using namespace std;
 
+// nodo albero con due figli
 struct node_t {
   int value;
   node_t *next[2];
 };
-
 typedef node_t *tree_t;
 
+// init nodo con puntatori ai figli su NULL
 node_t *new_node(int value)
 {
   node_t *node = new node_t;
@@ -20,6 +22,7 @@ node_t *new_node(int value)
   return node;
 }
 
+// riempi albero di *level* figli casuali
 void add_child(tree_t &tree, int level)
 {
   if (level == 0)
@@ -31,11 +34,12 @@ void add_child(tree_t &tree, int level)
   }
 }
 
-void show(tree_t tree, int level, int spaces, int current=0)
+// stampa albero tutti i figli ad altezza *level*
+void show(tree_t tree, int height, int spaces, int current=0)
 {
   if (tree == NULL)
     return;
-  if (level == current)
+  if (height == current)
   {
     for (int i=0; i<spaces; i++)
       cout << "  ";
@@ -45,22 +49,23 @@ void show(tree_t tree, int level, int spaces, int current=0)
     for (int i=0; i<spaces; i++)
       cout << "  ";
   }
-  else if(level > current)
+  else if(height > current)
     for (int i=0; i<2; i++)
-      show(tree->next[i], level, spaces, current+1);
+      show(tree->next[i], height, spaces, current+1);
 }
 
-
+// Stampa albero completo "ordinato"
 void show_tree(tree_t tree, const int LIVELLI)
 {
   for (int i=0; i<LIVELLI; i++)
     {
-
       show(tree, i, pow(2, LIVELLI-i-1)-1);
       cout << endl << endl;
     }
 }
 
+// cerca *value* ricorsivamente nell'albero
+// se value != nodo cerco nei nei figli 0 e 1
 bool search(tree_t tree, int value)
 {
   if (tree == NULL)
@@ -75,7 +80,7 @@ bool search(tree_t tree, int value)
 int main(int argc, char** argv)
 {
   srand(time(NULL));
-  const int LIVELLI = 6;
+  const int LIVELLI = 4;
 
   tree_t tree = NULL;
   add_child(tree, LIVELLI);
@@ -88,5 +93,7 @@ int main(int argc, char** argv)
     cout << "Numero presente nell'albero" << endl;
   else
     cout << "Il numero non è presente" << endl;
+
+  system("pause");
   return 0;
 }
--- DFS/tree.cpp
+++ DFS/tree.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <stdlib.h>
 #include <cmath>
+#include <ctime>
 
 using namespace std;
 
