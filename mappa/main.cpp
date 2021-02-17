66945bd649ffa14f7066c506d2a295405b83b3da
Tommaso Paoli <paoli7612@gmail.com>
2021-02-17 10:56:44 +0100

    mappa salva e carica funzionanti

--- mappa/main.cpp
+++ mappa/main.cpp
@@ -10,12 +10,15 @@ int main()
 {
   map_t map;
 
-  //map_new(map, coord_new(10, 10));
-  //map_fill(map, coord_new(0, 0), map.size, 2);
-  map_load(map, "test/spawn.dat");
-  cout << map.size.x << " " << map.size.y << endl;
+  map_new(map, coord_new(10, 10));
+  map_fill(map, coord_new(0, 0), map.size, 2);
+
+  map_save(map, "test/spawn.dat");
+
+  map_t map2;
+  map_load(map2, "test/spawn.dat");
+
   map_print(map, cout);
-  //map_save(map, "test/spawn.dat");
 
   char c;
   cin >> c;
--- mappa/map.cpp
+++ mappa/map.cpp
@@ -33,11 +33,11 @@ void map_save(const map_t &map, const char filename[])
 {
   std::ofstream file(filename);
 
-  coord_t size;
-  file.write((char*)&size, sizeof(coord_t));
+  file.write((char*)&map.size, sizeof(coord_t));
 
   for (int y=0; y<map.size.y; y++)
-    file.write((char*)&(map.tiles[y]), sizeof(tile_t[map.size.x]));
+    for (int x=0; x<map.size.x; x++)
+      file.write((char*)&(map.tiles[y][x]), sizeof(tile_t));
 
   file.close();
 }
@@ -48,12 +48,16 @@ void map_load(map_t &map, const char filename[])
   std::ifstream file(filename);
 
   coord_t size;
+  file.read((char*)&size, sizeof(coord_t));
+
   map_new(map, size);
 
-  for (int y=0; y<map.size.y; y++)
-    file.read((char*)&(map.tiles[y]), sizeof(tile_t[map.size.x]));
 
-  file.close();
+  for (int y=0; y<map.size.y; y++)
+    for (int x=0; x<map.size.x; x++)
+      file.read((char*)&(map.tiles[y][x]), sizeof(tile_t));
+
+
 }
 
  // riempi da un punto all'altro
