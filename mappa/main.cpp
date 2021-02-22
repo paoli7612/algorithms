4820288e72e1b80782e8478bd10ec7574666b32b
paoli7612 <paoli7612@gmail.com>
2021-02-22 19:28:44 +0100

    screen

--- mappa/main.cpp
+++ mappa/main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 #include "map.h"
 
+const int WIDTH = 50, HEIGHT = 50;
+
 int main()
 {
   srand(time(NULL));
@@ -15,26 +17,18 @@ int main()
 
   map_t map;
 
-  map_new(map, coord_new(10, 10));
+  map_new(map, coord_new(WIDTH, HEIGHT));
   tile_t tile;
   tile.sprite = BLOCK;
-  tile.code = -1;
+  tile.code = 0;
   map_fill(map, coord_new(0, 0), map.size, tile);
+  tile.code = -1;
+  map_fill(map, coord_new(0, 0), coord_new(1, HEIGHT), tile);
+  map_fill(map, coord_new(0, 0), coord_new(WIDTH, 1), tile);
+  map_fill(map, coord_new(WIDTH-1, 0), coord_new(WIDTH, HEIGHT), tile);
+  map_fill(map, coord_new(0, HEIGHT-1), coord_new(WIDTH, HEIGHT), tile);
   map_print(map, cout);
-  tile.code += 5;
-  map_fill(map, coord_new(x, y), coord_new(x+1, y+1), tile);
   map_save(map, "test/spawn.dat");
-
-
-  cout << endl;
-
-  map_t map2;
-  map_load(map2, "test/spawn.dat");
-
-  map_print(map2, cout);
-
-  char c;
-  cin >> c;
 
   return 0;
 }
