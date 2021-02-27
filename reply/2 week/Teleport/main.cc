fe39c41c1889874e820674b84ebd73230c6c58e0
Tommaso Paoli <paoli7612@gmail.com>
2021-02-27 11:01:35 +0100

    start teleport

---
+++ reply/2 week/Teleport/main.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	ifstream file("1.txt");
+	ofstream out("1.out.txt");
+	
+	int T;
+	file >> T;
+	for (int i=0; i<T; i++)
+	{
+		
+	}
+	
+	return 0;
+}
