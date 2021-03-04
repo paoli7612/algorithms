de4acb48c220ff3a1e33da46b4ac7a680a22301b
paoli7612 <paoli7612@gmail.com>
2021-03-04 12:42:58 +0100

    start quicksort

--- 004 Quick sort/main.cpp
+++ 004 Quick sort/main.cpp
@@ -2,8 +2,36 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
+#define N 10
+
+void print(const int *array, const int len)
 {
-	
+	for (int i = 0; i < len; i++)
+		cout << array[i] << "\t";
+	cout << endl;
 }
 
+void quick_sort(int *array, const int len)
+{
+
+}
+
+int main(void) {
+    srand(time(NULL));
+
+	int len = rand()%20 + 3;
+	int *array = new int[len];
+
+	array = new int[len];
+
+	for (int i=0; i<len; i++)
+		array[i] = rand()%40-20;
+
+	cout << "len: " << len << endl;
+
+	print(array, len);			//disordinato
+	quick_sort(array, len);	//riordino
+	print(array, len);			//ordinato
+
+	return 0;
+}
