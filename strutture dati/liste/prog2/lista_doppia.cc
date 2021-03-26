552de6e20fcd1e2292ebe7ccf43e37e2c570e38c
= <paoli7612@gmail.com>
2021-03-26 13:58:41 +0100

    lista doppia

---
+++ strutture dati/liste/prog2/lista_doppia.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "lista_doppia.h"
+
+using namespace std;
+
+typedef node_t *list_t;
+
+void stampa_lista(list_t lista)
+{
+    while (lista != NULL)
+    {
+
+    }
+}
---
+++ strutture dati/liste/prog2/lista_doppia.h
@@ -0,0 +1,7 @@
+struct node_t {
+    int value;
+    node_t *next;
+};
+typedef node_t *list_t;
+
+void stampa_lista(list_t);
