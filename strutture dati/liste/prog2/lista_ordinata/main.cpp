e3ca755f6575ec3403398193ab3197a3fa919785
= <paoli7612@gmail.com>
2021-04-07 15:15:59 +0200

    lista ordinata di numeri ok

--- strutture dati/liste/prog2/lista_ordinata/main.cpp
+++ strutture dati/liste/prog2/lista_ordinata/main.cpp
@@ -11,8 +11,30 @@ typedef node_t *list_t;
 
 list_t aggiungi(list_t list, node_t *node)
 {
+    if (list == NULL)
+        return node;
 
+    if (list->value > node->value)
+    {
+        node->next = list;
+        return node;
+    }
+    
+    node_t *ap = list;
+    while (ap->next != NULL)
+    {
+        if (ap->next->value > node->value)
+        {
+            node->next = ap->next;
+            ap->next = node;
+            return list;
+        }
+        ap = ap->next;
+    }
+    ap->next = node;
+    node->next = NULL;
 
+    return list;
 }
 
 void stampa(list_t list)
@@ -27,6 +49,15 @@ void stampa(list_t list)
 
 int main(int argc, char **argv)
 {
+    list_t list = NULL;
+    int a = 10;
+    while (a--)
+    {
+        node_t *node = new node_t;
+        cin >> node->value;
+        list = aggiungi(list, node);
+        stampa(list);
+    }
 
     return 0;
 }
