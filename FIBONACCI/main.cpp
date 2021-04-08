5d5dd73f04cb1ab5571b3074dc777434df2f5c12
Tommaso Paoli <paoli7612@gmail.com>
2021-04-08 14:50:02 +0200

    Create main.cpp

---
+++ FIBONACCI/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int n, int primo=1, int secondo=1, int c=2)
+{
+    if (c == n)
+        return secondo;
+
+    if (c == 2)
+        cout << primo << endl << secondo << endl;
+
+    int terzo = primo + secondo;
+    cout << terzo << endl;
+    return fibo(n, secondo, terzo, c+1);
+}
+
+int main(int argc, char **argv)
+{
+    cout << fibo(10);
+
+    return 0;
+}
