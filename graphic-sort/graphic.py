5f41feb766a515edf528f158532cf7c4288532ec
paoli7612 <paoli7612@gmail.com>
2021-04-09 14:41:09 +0200

    draw array

--- graphic-sort/graphic.py
+++ graphic-sort/graphic.py
@@ -2,23 +2,37 @@ import pygame
 
 class Window:
     def __init__(self):
-        self.screen = pygame.display.set_mode((200, 200))
+        pygame.init()
+        self.screen = pygame.display.set_mode((1000, 400))
         self.clock = pygame.time.Clock()
-        self.loop()
-
-    def loop(self):
-        self.running = True
-        while (self.running):
-            self.event()
-            self.update()
-            self.draw()
-            self.clock.tick(60)
         
+        self.font = pygame.font.Font('arial_narrow_7.ttf', 40)
+
+    def write(self, text, x, y):
+        surface = self.font.render(text, True, (255, 0, 0))
+        rect = surface.get_rect()
+        rect.topleft = (x, y)
+        self.screen.blit(surface, rect)
+
     def event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+
     def update(self):
         pass
-    def draw(self):
-        pass
+
+    
+    def sort(self, array):
+        self.running = True
+        while (self.running):
+            self.event()
+            self.update()
+
+            self.screen.fill((255, 255, 255))
+            for i, a in enumerate(array):
+                self.write(str(a), i*40+30, 20)
+                
+            pygame.display.flip()
+
+            self.clock.tick(60)
