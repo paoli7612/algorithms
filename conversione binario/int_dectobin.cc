da0ae66bf06c00e68ef2c777c003b592a98bf2c0
= <paoli7612@gmail.com>
2021-04-17 08:43:19 +0200

    convert char* to bin and calculate n bit

---
+++ conversione binario/int_dectobin.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int chartodec(const char *dec)
+{
+    int cifre = 0;
+    for (; dec[cifre]!='\0'; cifre++);
+
+    int decimale = 0;
+    for (int i=0; i<cifre; i++)
+        decimale += (dec[i] - '0') * pow(10, cifre-i-1);
+    
+    return decimale;
+}
+
+char* dec_to_bin(const char *dec)
+{
+
+    int decimale = chartodec(dec);
+    
+    int bit = 1;
+    for (; pow(2, bit)<decimale; bit++);
+
+    char* bin = new char[bit];
+
+
+
+    return bin;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    if (argc != 2)
+    {
+        cerr << "1 argomento: [decimale]" << endl;
+        return 1;
+    }
+
+    const char *dec = argv[1];
+    char* bin = dec_to_bin(dec);
+
+    cout << dec << " -> " << bin << endl;
+
+    return 0;
+}
