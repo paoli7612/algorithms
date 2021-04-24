ff198abf581994f4a1cfd0841f84e8ecca5956c6
= <paoli7612@gmail.com>
2021-04-24 08:38:40 +0200

    menu opzioni

--- strutture dati/liste/prog2/es1/main.cpp
+++ strutture dati/liste/prog2/es1/main.cpp
@@ -6,28 +6,52 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    list_t lista = NULL;
-    stampa(lista);
 
-    for (int i=0; i<10; i++)
-        lista = aggiungi_testa(lista, i);
+    const char menu[] =
+        "1. aggiungi in testa\n"
+        "2. aggiungi in coda\n"
+        "3. aggiungi dopo elemento\n"
+        "4. inserisci a posizion\n"
+        "5. rimuovi posizione\n"
+        "6. elimina testa\n"
+        "7. elimina coda\n"
+        "8. elimina posizione\n"
+        "9. elimina lista\n"
+        "10. <!> termina programma";
 
-    stampa(lista);
+    bool running = true;
+    while (running)
+    {
+        cout << menu << endl;
 
+        int scelta;
+        cin >> scelta;
 
-    for (int i=0; i<10; i++)
-        lista = aggiungi_coda(lista, i);
-
-    stampa(lista);
-
-    lista = dealloca_coda(lista);
-
-    stampa(lista);
-
-    lista = dealloca_testa(lista);
-    
-    stampa(lista);
-
+        switch (scelta)
+        {
+        case 1: // aggiungi in testa
+            break;
+        case 2: // aggiungi in coda
+            break;
+        case 3: // aggiungi dopo elemento
+            break;
+        case 4: // inserisci a posizion
+            break;
+        case 5: // rimuovi posizione
+            break;
+        case 6: // elimina testa
+            break;
+        case 7: // elimina coda
+            break;
+        case 8: // elimina posizione
+            break;
+        case 9: // elimina lista
+            break;
+        case 10: // termina programma
+            
+            break;
+        }
+    }
 
     return 0;
 }
