4810e963c92155c72322afda7b120870b65a82db
= <paoli7612@gmail.com>
2021-04-24 08:42:38 +0200

    aggiungi in testa e in coda cli

--- strutture dati/liste/prog2/es1/main.cpp
+++ strutture dati/liste/prog2/es1/main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int main(int argc, char **argv)
 {
 
+    list_t lista = NULL;
+
     const char menu[] =
         "1. aggiungi in testa\n"
         "2. aggiungi in coda\n"
@@ -22,34 +24,50 @@ int main(int argc, char **argv)
     bool running = true;
     while (running)
     {
+        stampa(lista);
         cout << menu << endl;
+
 
         int scelta;
         cin >> scelta;
 
         switch (scelta)
         {
-        case 1: // aggiungi in testa
-            break;
-        case 2: // aggiungi in coda
-            break;
-        case 3: // aggiungi dopo elemento
-            break;
-        case 4: // inserisci a posizion
-            break;
-        case 5: // rimuovi posizione
-            break;
-        case 6: // elimina testa
-            break;
-        case 7: // elimina coda
-            break;
-        case 8: // elimina posizione
-            break;
-        case 9: // elimina lista
-            break;
-        case 10: // termina programma
-            
-            break;
+            case 1: // aggiungi in testa
+            {
+                int i;
+                cout << "inserisci numero da aggiungere in testa alla lista: ";
+                cin >> i;
+
+                lista = aggiungi_testa(lista, i);
+            }
+                break;
+            case 2: // aggiungi in coda
+            {
+                int i;
+                cout << "inserisci numero da aggiungere in coda alla lista: ";
+                cin >> i;
+
+                lista = aggiungi_coda(lista, i);
+            }
+                break;
+            case 3: // aggiungi dopo elemento
+                break;
+            case 4: // inserisci a posizion
+                break;
+            case 5: // rimuovi posizione
+                break;
+            case 6: // elimina testa
+                break;
+            case 7: // elimina coda
+                break;
+            case 8: // elimina posizione
+                break;
+            case 9: // elimina lista
+                break;
+            case 10: // termina programma
+                
+                break;
         }
     }
 
