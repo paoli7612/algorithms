085f95d0c33360095fc526aae4d453dbf280eb3e
paoli7612 <paoli7612@gmail.com>
2021-05-05 20:12:47 +0200

    add next and back

--- strutture dati/liste/listedoppie/liste.cc
+++ strutture dati/liste/listedoppie/liste.cc
@@ -44,6 +44,25 @@ void add_next(list_t &list, int value)
     };
 }
 
+void add_back(list_t &list, int value)
+{
+    if (list == NULL)
+    {
+        list = new node_t {value, NULL, NULL};
+        return;
+    }
+
+    node_t *ap = list;
+    while (ap->back != NULL)
+        ap = ap->back;
+
+    ap->back = new node_t {
+        value,  // value
+        NULL,   // back
+        ap    // next
+    };
+}
+
 list_t last(list_t list)
 {
     if (list == NULL)
@@ -51,6 +70,17 @@ list_t last(list_t list)
 
     while (list->next != NULL)
         list = list->next;
+    
+    return list;
+}
+
+list_t first(list_t list)
+{
+    if (list == NULL)
+        return NULL;
+
+    while (list->back != NULL)
+        list = list->back;
     
     return list;
 }
--- strutture dati/liste/listedoppie/main.cc
+++ strutture dati/liste/listedoppie/main.cc
@@ -7,14 +7,40 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    list_t primo = new node_t {0, NULL, NULL};
-    add_next(primo, 2);
-    add_next(primo, 4);
-    add_next(primo, 6);
-    list_t ultimo = last(primo);
+
+    list_t list = NULL;
     
-    print_next(primo);
-    print_back(ultimo);
+    bool running = true;
+    while (running)
+    {
+        cout << 
+            "\t 1. add next" <<
+            "\t 2. add back" << endl;
+
+        int choice;
+        cin >> choice;
+
+        switch (choice)
+        {
+            int value;
+        case 1: // add next
+            cout << "value: ";
+            cin >> value;
+            add_next(list, value);
+            break;
+
+        case 2: // add back
+            cout << "value: ";
+            cin >> value;
+            add_back(list, value);
+            break;
+        
+        default:
+            break;
+        }
+
+        print_next(first(list));
+    }
 
     return 0;
 }
