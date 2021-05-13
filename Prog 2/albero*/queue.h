3af5b1fee807f2fa7ccb7f685738aacce3d6301b
paoli7612 <paoli7612@gmail.com>
2021-05-13 20:20:17 +0200

    next

--- Prog 2/albero*/queue.h
+++ Prog 2/albero*/queue.h
@@ -1,4 +1,11 @@
 #ifndef INCLUDE_QUEUE_H_
 #define INCLUDE_QUEUE_H_
 
+#include "tree.h"
+
+struct QueueNode_t {
+    QueueNode_t *next;
+    
+};
+
 #endif /* INCLUDE_QUEUE_H_ */
--- Prog 2/albero*/tree.h
+++ Prog 2/albero*/tree.h
@@ -1,14 +1,14 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
-struct node_tree_t {
+struct TreeNode_t {
     int value;
-    node_tree_t *nextChild;
-    node_tree_t *nextSiblings;
-    node_tree_t *parent;
+    TreeNode_t *nextChild;
+    TreeNode_t *nextSiblings;
+    TreeNode_t *parent;
 };
 
-typedef node_tree_t *tree_t;
+typedef TreeNode_t *tree_t;
 
 
 #endif /* INCLUDE_TREE_H_ */
