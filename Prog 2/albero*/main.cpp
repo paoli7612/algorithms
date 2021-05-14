166378a8c8a9921f0784e0b53ead25d716189654
paoli7612 <paoli7612@gmail.com>
2021-05-14 21:11:46 +0200

    use print preorder

--- Prog 2/albero*/main.cpp
+++ Prog 2/albero*/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-
-    Tree_t t = new_node(12, NULL, NULL);
+    Tree_t t = new_node(12, NULL,
+        new_node(12, NULL, NULL));
 
     print(t, PREORDER);
 
