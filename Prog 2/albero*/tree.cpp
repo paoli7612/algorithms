7859a47ae6176dd45051dd8d04b32d574cae4c86
paoli7612 <paoli7612@gmail.com>
2021-05-15 08:53:03 +0200

    alla faccia

--- Prog 2/albero*/tree.cpp
+++ Prog 2/albero*/tree.cpp
@@ -17,10 +17,9 @@ void print_preorder(Tree_t tree)
         return;
 
     std::cout << tree->value << " ";
-    
+
     TreeNode_t *ap = tree->firstChild;
-    if (ap == NULL)
-        std::cout << "\b";
+    if (ap == NULL) { std::cout << "\b"; return; }
     while (ap != NULL)
     {
         std::cout << "(";
@@ -28,7 +27,23 @@ void print_preorder(Tree_t tree)
         ap = ap->nextSiblings;
         std::cout << ")";
     }
-    
+}
+
+void print_postorder(Tree_t tree)
+{
+    if (tree == NULL)
+        return;
+
+    TreeNode_t *ap = tree->firstChild;
+    while (ap != NULL)
+    {
+        std::cout << "(";
+        print_preorder(ap);
+        ap = ap->nextSiblings;
+        std::cout << ")";
+    }
+
+    std::cout << tree->value << " ";
 
 }
 
@@ -39,8 +54,40 @@ void print(Tree_t tree, const print_mode_t mode)
     case PREORDER:
         print_preorder(tree);
         break;
-    
+    case POSTORDER:
+        print_postorder(tree);
+        break;
+
     default:
         break;
+    }
+    std::cout << std::endl;
+}
+
+int width(Tree_t tree)
+{
+    if (tree == NULL)  
+        return 0;
+
+    int w = 0;
+    for (TreeNode_t *ap=tree->firstChild; ap!=NULL; ap=ap->nextSiblings)
+        w += width(ap);
+    return w + 1;
+}
+
+void print_graphic(Tree_t tree)
+{  
+    for (int i=0; i<width(tree); i++)
+        std::cout << "  ";
+    std::cout << tree->value;
+    for (int i=0; i<width(tree); i++)
+        std::cout << " ";
+
+
+    if (tree->nextSiblings != NULL)
+        print_graphic(tree->nextSiblings);
+    if (tree->firstChild != NULL) {
+        std::cout << std::endl;
+        print_graphic(tree->firstChild);
     }
 }
