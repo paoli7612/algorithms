3adf1ceea6f869c3adc18603bbaaee074fe3302e
paoli7612 <paoli7612@gmail.com>
2021-05-17 05:27:07 +0200

    random and concat

--- Prog 2/lista/main.cpp
+++ Prog 2/lista/main.cpp
@@ -7,13 +7,12 @@ using namespace std;
 int main(int argc, char **argv)
 {
 
-    List_t l1 = random_list_new(10);
-    List_t l2 = random_list_new(10);
-    print(l1);
-    print(l2);
-
-    concat(l1, l2);
-    print(l1);
+    List_t l = random_list_new(10);
+    print(l);
+    random_list_edit(l);
+    print(l);
+    concat(l, random_list_new(10));
+    print(l);
 
     return 0;
 }
