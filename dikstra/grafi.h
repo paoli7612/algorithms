6de458b1877c09c14abafd85b71a9926cedebe32
paoli7612 <paoli7612@gmail.com>
2021-05-21 20:39:07 +0200

    archi

---
+++ dikstra/grafi.h
@@ -0,0 +1,17 @@
+
+struct Node_t {
+    Node_t *next;
+    int vertice;
+    float peso;
+};
+
+typedef Node_t *List_t;
+
+struct Grafo_t {
+    int n_vertici;
+    List_t *archi;
+};
+
+void init_grafo(Grafo_t &grafo, const int vertici);
+void nuovo_arco(Grafo_t $grafo, int a, int b);
+void stampa_grafo(const Grafo_t &grafo);
