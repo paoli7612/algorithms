77bc2c230eeaece988bbaf48c5d2ca41a7d88e5c
paoli7612 <paoli7612@gmail.com>
2021-05-21 20:42:54 +0200

    mancano liste func

--- dikstra/grafi.cpp
+++ dikstra/grafi.cpp
@@ -5,3 +5,15 @@ void init_grafo(Grafo_t &grafo, const int vertici)
     grafo.n_vertici = vertici;
     grafo.archi = new List_t[vertici];
 }
+
+void nuovo_arco(Grafo_t &grafo, int a, int b, float peso)
+{
+
+}
+
+void stampa_grafo(const Grafo_t &grafo)
+{
+    std::cout << grafo.n_vertici << std::endl;
+    for (int i=0; i<grafo.n_vertici; i++)
+        std::cout << i << std::endl;
+}
--- dikstra/grafi.h
+++ dikstra/grafi.h
@@ -1,3 +1,4 @@
+#include <iostream>
 
 struct Node_t {
     Node_t *next;
@@ -13,5 +14,5 @@ struct Grafo_t {
 };
 
 void init_grafo(Grafo_t &grafo, const int vertici);
-void nuovo_arco(Grafo_t $grafo, int a, int b);
+void nuovo_arco(Grafo_t &grafo, int a, int b, float peso);
 void stampa_grafo(const Grafo_t &grafo);
