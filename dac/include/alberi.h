69f654b913bde11f8dac92c458358b806627b91c
paoli7612 <paoli7612@gmail.com>
2021-05-22 08:09:12 +0200

    struct albero e grafo

--- dac/include/alberi.h
+++ dac/include/alberi.h
@@ -1,2 +1,11 @@
 #include "../include/alberi.h"
 
+
+struct NodoAlbero_t {
+
+    int value;
+
+    NodoAlbero_t *primoFiglio;
+    NodoAlbero_t *prossimoFratello;
+    NodoAlbero_t *padre;
+};
---
+++ dac/include/grafi.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <fstream>
+
+struct Grafo_t {
+    
+};
