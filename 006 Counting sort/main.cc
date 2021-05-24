507636795db91416dfea03bb8eba0da712caa091
paoli7612 <paoli7612@gmail.com>
2021-05-24 17:54:37 +0200

    prepare counting sort

--- 006 Counting sort/main.cc
+++ 006 Counting sort/main.cc
@@ -2,44 +2,34 @@
 
 using namespace std;
 
-void stampa(float array[], const int len)
+void scambia(int &a, int &b)
+{
+    a += b;
+    b = a-b;
+    a = a-b;
+}
+
+void counting(int *array, int n)
+{
+}
+
+void stampa(int *array, int len)
 {
     for (int i=0; i<len; i++)
         cout << array[i] << " ";
     cout << endl;
 }
 
-// step (per esempio 0.5 tra [2, 2.5, 3, 3.5]) => intervallo minimo tra le chiavi
-void counting_sort(float array[], const int len, const int max, const float step)
-{
-    int l_occ = max/step;
-
-    int *occ = new int[l_occ];
-    for (int i=0; i<l_occ; i++)
-        occ[i] = 0;
-    
-    float prodotto = 1/step;
-
-    for (int i=0; i<len; i++)
-        occ[(int)(array[i]*prodotto)]++;
-
-    int index = 0;
-    for (int i=0; i<l_occ; i++)
-        while (occ[i]>0)
-        {
-            array[index++] = i/prodotto;
-            occ[i]--;
-
-        }
-}
-
 int main(int argc, char **argv)
 {
-    int len = 7;
-    float array[] = {1.1, 1.4, 1.6, 0.1, 0.5, 0.5, 1.9};
+
+    int array[] = {3, 7, 7, 2, 3, 5, 4, 0, 2, 8};
+    int len = 10;
 
     stampa(array, len);
-    counting_sort(array, len, 2, 0.1);
+
+    counting(array, 10);
+
     stampa(array, len);
 
     return 0;
