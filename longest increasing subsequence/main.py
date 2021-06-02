d7e9a4987722356c0abf56023bff9d5791b6037c
paoli7612 <paoli7612@gmail.com>
2021-06-02 14:04:10 +0200

    per ogni valore ci stama gli indici dei minori

--- longest increasing subsequence/main.py
+++ longest increasing subsequence/main.py
@@ -1,11 +1,14 @@
 a = [32, 32, 30, 6, 28, 17, 19, 4, 15, 25, 34, 15]
 l = [ 1,  1,  1, 1,  1,  1,  1, 1,  1,  1,  1,  1]
 
-for x, i in enumerate(a[1:]):
-    for y, j in enumerate(a):
-        if i < j:
-            l[x] += l[y]
-        else:
-            break
+def mins(a, v):
+    l = list()
+    for x, i in enumerate(a):
+        if i < v:
+            l += [x]
+    return l
 
-print(l)
+for x, i in enumerate(a[1:]):
+    print(x, i)
+    print(mins(a[0:x+1], i))
+    print()
