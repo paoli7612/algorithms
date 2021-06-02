6efde6fbc7ee420eaefc5bfb6081f8c5006e8a61
paoli7612 <paoli7612@gmail.com>
2021-06-02 13:13:00 +0200

    ni

--- longest increasing subsequence/main.py
+++ longest increasing subsequence/main.py
@@ -1,9 +1,14 @@
 a = [30, 34, 11, 34, 5, 30, 17, 13, 12, 8, 30, 18]
+l = [ 1,  1,  1,  1, 1,  1,  1,  1,  1, 1,  1,  1]
 
-for i in a[1:]:
-    for j in a:
-        print(str(i)+"-"+str(j)+", ", end="")
-    print()
+for x, i in enumerate(a[1:]):
+    for y, j in enumerate(a):
+        if i < j:
+            l[x] += l[y]
+        else:
+            break
+
+print(l)
 
 # L = [1, 2, 1, 2, 1, 2, 2, 2, 2, 2, 3, 3]
 # max(L) -> 3 -> LONGEST INCREASING SUBSEQUENCE
