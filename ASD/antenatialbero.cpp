2c2a4677fa5d2a18b57f056bf20e8d5f895149df
paoli7612 <paoli7612@gmail.com>
2021-06-05 15:11:24 +0200

    stampa antenati

--- ASD/antenatialbero.cpp
+++ ASD/antenatialbero.cpp
@@ -16,20 +16,65 @@ node_t *new_node(int value, node_t *left=NULL, node_t *right=NULL)
     };
 }
 
-void order(tree_t tree)
+void inorder(tree_t tree)
 {
     if (tree == NULL)
         return;
     
-    order(tree->left);
+    inorder(tree->left);
     cout << " " << tree->value << " ";
-    order(tree->right);
+    inorder(tree->right);
+}
+
+bool foo(tree_t tree, node_t *node=NULL)
+{
+    if (tree == NULL)
+        return false;
+    
+    if (tree == node)
+        return true;
+    
+    if (foo(tree->left))
+        cout << tree->value << endl;
+    else if (foo(tree->right))
+        cout << tree->value << " ";
+    
+    return true;
 }
 
 int main(int argc, char **argv)
 {
-    tree_t tree = new_node(1, new_node(2, new_node(4, new_node(8), new_node(9)), new_node(5, new_node(10), new_node(11))), new_node(3, new_node(6), new_node(7)));
-    order(tree);
+    tree_t tree = 
+    new_node(9,
+        new_node(8,
+            new_node(6,
+                new_node(2),
+                new_node(1)
+                ),
+            new_node(5,
+                new_node(0),
+                new_node(1)
+                )
+            ),
+        new_node(7,
+            new_node(4,
+                new_node(2),
+                new_node(3)
+                ),
+            new_node(3,
+                new_node(4),
+                new_node(5)
+                )
+            )
+        );
+    inorder(tree);
+    cout << endl;
+
+    node_t *n = tree->left->right->right;
+
+    cout << n->value << endl;
+
+    foo(tree, n);
 
     return 0;
 }
