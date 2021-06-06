54296a61273b0ac7cb00e15e8273e00f789e1bab
Tommaso Paoli <paoli7612@gmail.com>
2021-06-06 05:08:36 +0200

    media albero finita

--- ASD/mediaalbero.cpp
+++ ASD/mediaalbero.cpp
@@ -14,16 +14,26 @@ node_t *n(int value, node_t *left=NULL, node_t *right=NULL)
     return new node_t {value, left, right};
 }
 
+float media(tree_t tree, int &somma, int &n)
+{
+    if (tree == NULL)
+        return 0;
+
+    n++;
+    somma += tree->value;
+
+    media(tree->left, somma, n);
+    media(tree->right, somma, n);
+
+    return ((float)somma)/n;
+}
+
 int main(int argc, char **argv)
 {
 
     tree_t tree = n(1, n(2, n(4), n(5)), n(3, n(6), n(7)));
-
-    while (tree != NULL)
-    {
-        cout << tree->value;
-        tree = tree->left;
-    }
+    int s=0, n=0;
+    cout << media(tree, s, n);
 
     return 0;
 }
