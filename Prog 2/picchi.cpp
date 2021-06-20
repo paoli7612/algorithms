86485e27baf04718a272d0a1209968c7a3d7fa30
= <paoli7612@gmail.com>
2021-06-20 12:23:59 +0200

    start piccchi

---
+++ Prog 2/picchi.cpp
@@ -0,0 +1,23 @@
+/*Un picco di una sequenza S di valori è un elemento strettamente maggiore dei suoi due
+elementi contigui o maggiore dell’elemento contiguo, se l’altro manca.
+Esempio: Nella sequenza [4, 3, 3, 3, 0, −1, 3, −3, 4, 2], 4, 3 e 4 sono picchi.
+Data una sequenza di valori interi memorizzati in una lista doppia dichiarata sotto, si scriva la
+funzione ricorsiva int picchi(lista) che restituisce il numero di picchi contenuti nella
+lista.
+Esempio: Sulla sequenza [4, 3, 3, 3, 0, −1, 3, −3, 4, 2] deve restituire 3.
+struct elem
+{
+ int inf;
+ elem* pun ;
+ elem* prev;
+} ;
+typedef elem* lista ;*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+}
