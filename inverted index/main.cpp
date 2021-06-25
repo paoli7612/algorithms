3cb5223f64352610e8877b67e4026fd674c90991
paoli7612 <paoli7612@gmail.com>
2021-06-25 08:24:40 +0200

    convert char in char[20]

--- inverted index/main.cpp
+++ inverted index/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 struct NodeList {
-    char filename;
+    char filename[20];
     NodeList *next;
 };
 typedef NodeList *List;
@@ -24,18 +25,21 @@ void list_add(List &list, NodeList *node)
     else
         list_add(list->next, node);
 }
-void list_add(List &list, char filename)
+void list_add(List &list, const char filename[20])
 {
-    list_add(list, new NodeList{ filename, NULL });
+    NodeList *node = new NodeList;
+    strcpy(node->filename, filename);
+    node->next = NULL;
+    list_add(list, node);
 }
 
 int main(int argc, char **argv)
 {
     List posting_list = NULL;
 
-    list_add(posting_list, 'a');
-    list_add(posting_list, 'b');
-    list_add(posting_list, 'c');
+    list_add(posting_list, "primo");
+    list_add(posting_list, "secondo");
+    list_add(posting_list, "terzo");
     list_print(posting_list);
 
     return 0;
