c01491f7167ffe37d74e62903bbdbcb1d3702c4c
paoli7612 <paoli7612@gmail.com>
2021-06-30 19:27:25 +0200

    leggi e stampa riga da istream c++

---
+++ leggi riga/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+void leggi_riga(istream &is, char buffer[80], int maxlen=80)
+{
+    int i = 0;
+    do is.read(&buffer[i++], 1);
+    while (buffer[i] != '\n');
+}
+
+void stampa_riga(char *buffer, int maxlen=80)
+{
+    for (int i=0; i<maxlen && buffer[i]!='\n'; i++)
+        cout << *(&buffer[i]);
+}
+
+int main(int argc, char const *argv[])
+{
+    ifstream file("file.txt");
+
+    char buffer[80];
+
+    leggi_riga(file, buffer);
+    stampa_riga(buffer);
+    
+    return 0;
+}
