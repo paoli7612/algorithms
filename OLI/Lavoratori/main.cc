a770bae1388c2d5d38ab83678a2d402ec14d93ff
paoli7612 <paoli7612@gmail.com>
2022-02-06 11:07:34 +0100

    OLI lavoratori 1

---
+++ OLI/Lavoratori/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    const int N = 2; // matrice lavoratori
+
+    int m[N][N];
+
+    chiedi(m);
+    stampa(m);
+
+    return 0;
+}
