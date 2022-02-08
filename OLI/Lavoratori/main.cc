d16f601fc9ddf4ec9494eea1453e64847f49298d
paoli7612 <paoli7612@gmail.com>
2022-02-09 00:27:23 +0100

    naafaccio

--- OLI/Lavoratori/main.cc
+++ OLI/Lavoratori/main.cc
@@ -1,98 +1,104 @@
 #include <iostream>
 #include <cmath>
 #include <ctime>
+#include <fstream>
 
-struct matrice_t {
-    int **valori;
-    int n;
-};
+using namespace std;
 
-struct array_t {
-    int *valori;
-    int n;
-};
-
-struct pos_t {
+struct matrix_t {
     int x, y;
+    int **v;
+    int s = 0;
 };
 
-void inizializza(pos_t &pos, const int x, const int y) {
-    pos.x = x;
-    pos.y = y;
-}
+void init(matrix_t &matrix, const int x, const int y) {
+    matrix.y = y;
+    matrix.x = x;
 
-void inizializza(array_t &array, const int N) {
-    array.valori = new int[N];
-    array.n = N;
-    for (int i=0; i<N; i++) 
-        array.valori[i] = 0;
-}
-
-void inizializza(matrice_t &mat, const int N) {
-    mat.n = N;
-    mat.valori = new int*[N];
-    for (int i=0; i<N; i++) 
-        mat.valori[i] = new int[N];
-}
-
-void chiedi(matrice_t &mat, bool random) {
-    for (int y=0; y<mat.n; y++) {
-        for (int x=0; x<mat.n; x++) {
-            if (random) {
-                mat.valori[y][x] = rand()%10;
-            } else {
-                std::cout << "lavoratore " << y << "\tcompito " << x << ": "; 
-                std::cin >> mat.valori[y][x];
-            }
+    matrix.v = new int*[y];
+    for (int y=0; y<matrix.y; y++) {
+        matrix.v[y] = new int[matrix.x];
+        for (int x=0; x<matrix.x; x++) {
+            matrix.v[y][x] = 0;
         }
     }
 }
 
-void stampa(const array_t &array) {
-    for (int i=0; i<array.n; i++) 
-        std::cout << array.valori[i] << " ";
-    std::cout << std::endl; 
-}
-
-void stampa(const matrice_t &mat) {
-    for (int y=0; y<mat.n; y++) {
-        for (int x=0; x<mat.n; x++) {
-            if (x == 0) std::cout << y << ":\t";
-            std::cout << mat.valori[y][x] << " ";
-        }
-        std::cout << std::endl;
+void print(const matrix_t &matrix) {
+    cout << "print __ " << endl;
+    for (int y=0; y<matrix.y; y++) {
+        for (int x=0; x<matrix.x; x++) 
+            cout << matrix.v[y][x] << " ";
+        cout << endl;
     }
 }
 
-void calcola_soluzioni(matrice_t &matrice, array_t &array) {
+void ask(matrix_t &matrix, const bool random, istream &is=cin) {
+    for (int y=0; y<matrix.y; y++) 
+        for (int x=0; x<matrix.x; x++) {
+            if (random) matrix.v[y][x] = rand()%10;
+            else is >> matrix.v[y][x];
+        }
+}
 
+int s=0;
 
+void calculate(const matrix_t &matrix, matrix_t &solution, int x=-1, int y=-1) {
+    if (x == -1 && y == -1) {
+        for (int i=0; i<matrix.x; i++) {
+            calculate(matrix, solution, i, 0);
+        }
+    } else if (matrix.x - y == 1) {
+        cout << x << " " << y << endl;
+    } else {
+        cout << "asd" << y << endl;
+        for (int i=0; i<matrix.x; i++) {
+            if (i != x)
+                calculate(matrix, solution, i, y+1);
+        }
+    }
+}
 
+void load(matrix_t &matrix) {
+    ifstream file("matrix.txt");
+
+    int N;
+    file >> N;
+    init(matrix, N, N);
+    ask(matrix, false, file);
+
+    file.close();
 }
 
 int main(int argc, char const *argv[])
 {
+    matrix_t matrix;    
     srand(time(NULL));
-    matrice_t mat;
+
     int N;
     if (argc == 2) {
         N = std::atoi(argv[1]);
-        inizializza(mat, N);
-        chiedi(mat, true); 
+        if (N == 0) {
+            load(matrix);
+        } else {
+            init(matrix, N, N);
+            ask(matrix, true); 
+        }
     } else {
         std::cout << "numero lavoratori: ";
         std::cin >> N;
-        inizializza(mat, N);
-        chiedi(mat, false); 
+        init(matrix, N, N);
+        ask(matrix, false); 
     }
-    stampa(mat);
+    print(matrix);
 
-    matrice_t sol;
-    inizializza(sol, std::tgamma(mat.n+1));
+    matrix_t solution;
+    init(solution, matrix.x, tgamma(matrix.x+1));
 
-    calcola_soluzioni(mat, sol);
+    calculate(matrix, solution);
 
-    stampa(sol);
+    print(solution);
+
 
     return 0;
 }
